8d5137ea7961862e7f951285c62b5327ce6e3617
herkanuzun <herkanuzun@gmail.com>
2023-12-17 18:29:33 +0300

    Add script to all pages

--- js/index.js
+++ js/index.js
@@ -1,8 +1,8 @@
 const footerLinksBtn = document.getElementById("footerLinksBtn");
 const footerLinkList = document.getElementById("footerLinkList");
 
-const menuHamburger = document.querySelector(".menu-hamburger")
-const navLinks = document.querySelector(".nav-links")
+const menuHamburger = document.querySelector(".menu-hamburger");
+const navLinks = document.querySelector(".nav-links");
 
 footerLinksBtn.addEventListener("click",function(){
 
@@ -27,10 +27,23 @@ var mySwiper = new Swiper ('.swiper-container', {
       reverseDirection: false,
       disableOnInteraction: false,
   },
-})
+});
 
 
 
 menuHamburger.addEventListener('click',()=>{
 navLinks.classList.toggle('mobile-menu')
-})
+});
+
+
+
+window.addEventListener('scroll', function() {
+  var navbar = document.querySelector('.navbar');
+  if (window.scrollY > 100) { // Örneğin, 100 piksel aşağı kaydırdığınızda rengi değiştirelim
+    navbar.style.backgroundColor = '#50A55B'; // Yeni arka plan rengi
+    document.getElementById('logo').src='/img/Pine_Logo_White .png';
+  } else {
+    navbar.style.backgroundColor = '#eeeeee'; // Başlangıç rengi
+    document.getElementById('logo').src='/img/Pine_Logo_Green.png';
+  }
+});
